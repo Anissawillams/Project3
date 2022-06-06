dffab94a24a6101bcdf5c74ce7a6fcfcb45930d0
Abigail709 <101914320+Abigail709@users.noreply.github.com>
2022-06-06 18:49:08 -0500

    server push

--- server/server.js
+++ server/server.js
@@ -1,26 +1,28 @@
 const express = require('express')
+
 const app = express();
 
 const cors = require('cors');
-const { Server } = require('http');
 
 app.use = express();
+require('dotenv').config();
+
 
 
 const httpServer = require("http").createServer();
 const PORT = process.env.PORT || 3001;
 
-const io = require("socket.io")(httpServer, {
-    cors: {
-        origin:'http://localhost:3001',
-        methods: ['GET', 'POST']
-    }
-})
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-Server.listen(PORT, ()=> {
-    console.log('PORT is listening')
+app.get('/', (reg, res) => {
+    res.send('Messaging App')
 })
+
+
+Server.listen(PORT, ()=> {
+    console.log(`Now listening on port: ${PORT}`)
+});
